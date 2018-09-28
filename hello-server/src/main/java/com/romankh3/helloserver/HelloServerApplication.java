4d7c25f8d60119f57d261b3bdae38321489ab490
romankh3 <roman.beskrovnyy@gmail.com>
2018-09-28 18:07:15 +0300

    added eureka-service

--- hello-server/src/main/java/com/romankh3/helloserver/HelloServerApplication.java
+++ hello-server/src/main/java/com/romankh3/helloserver/HelloServerApplication.java
@@ -2,7 +2,10 @@ package com.romankh3.helloserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HelloServerApplication {
 
