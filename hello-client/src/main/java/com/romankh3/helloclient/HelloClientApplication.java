9f064e00b365032642157506914651a9a8b2fe74
romankh3 <roman.beskrovnyy@gmail.com>
2018-09-28 17:42:15 +0300

    Added Eureka Service, Hello Client, Hello Server.

---
+++ hello-client/src/main/java/com/romankh3/helloclient/HelloClientApplication.java
@@ -0,0 +1,12 @@
+package com.romankh3.helloclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloClientApplication.class, args);
+    }
+}
---
+++ hello-server/src/main/java/com/romankh3/helloserver/HelloServerApplication.java
@@ -0,0 +1,12 @@
+package com.romankh3.helloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServerApplication.class, args);
+    }
+}
